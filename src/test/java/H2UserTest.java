87aa8d40105a978f59a2b314bcaeac834f12276b
j0nas <jonas.jensen@msn.com>
2015-11-26 02:24:01 +0100

    Tests done

--- src/test/java/H2UserTest.java
+++ src/test/java/H2UserTest.java
@@ -10,7 +10,6 @@ import static org.junit.Assert.*;
  * Created by Jonas on 21.11.2015.
  */
 public class H2UserTest {
-
     private H2User h2User = new H2User();
     private String email = "jonas.jensen@msn.com";
     private String password = "abcdef";
@@ -20,7 +19,6 @@ public class H2UserTest {
     @After
     public void tearDown() throws Exception {
         h2User.delete(user.getId());
-
     }
 
     @Before
@@ -37,49 +35,22 @@ public class H2UserTest {
 
     @Test
     public void testUpdate() throws Exception {
-        H2User h2User = new H2User();
-        String email = "jonas.jensen@msn.com";
-        String password = "abcdef";
-        UserType type = UserType.TEACHER;
-        User user = h2User.create(email, password, type);
-
-        assertEquals(email, user.getEmail());
-        assertEquals(password, user.getPassword());
-        assertEquals(type, user.getType());
-
         user.setEmail("changed@email.com");
         user.setPassword("changedPassword");
         user.setType(UserType.STUDENT);
         assertTrue(h2User.update(user));
-
-        h2User.delete(user.getId());
     }
 
     @Test
     public void testFind() throws Exception {
-        H2User h2User = new H2User();
-
-        String email = "jonas.jensen@msn.com";
-        String password = "abcdef";
-        UserType type = UserType.TEACHER;
-        User user = h2User.create(email, password, type);
-
         User foundUser = h2User.find(user.getId());
         assertEquals(email, foundUser.getEmail());
         assertEquals(password, foundUser.getPassword());
         assertEquals(type, foundUser.getType());
-
-        h2User.delete(user.getId());
     }
 
     @Test
     public void testGetAll() throws Exception {
-        H2User h2User = new H2User();
-
-        String email = "jonas.jensen@msn.com";
-        String password = "abcdef";
-        UserType type = UserType.TEACHER;
-
         int userCount = 10;
         User[] users = new User[userCount];
         for (int i = 0; i < users.length; i++) {
@@ -88,8 +59,9 @@ public class H2UserTest {
 
         List<User> all = h2User.getAll();
         assertNotNull(all);
-        assertEquals(userCount, all.size());
-        for (int i = 0; i < all.size(); i++) {
+        assertEquals(userCount + 1, all.size());
+
+        for (int i = 0; i < userCount; i++) {
             assertEquals(email, users[i].getEmail());
             assertEquals(password, users[i].getPassword());
             assertEquals(type, users[i].getType());
@@ -102,5 +74,6 @@ public class H2UserTest {
 
     @Test
     public void testDelete() throws Exception {
+        assertTrue(h2User.delete(user.getId()));
     }
 }
