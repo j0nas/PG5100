4d2e1b35afde83e8d27357648e2cf23aaf731614
j0nas <jonas.jensen@msn.com>
2015-11-30 17:12:12 +0100

    userdao

---
+++ src/main/java/h2UserDao.java
@@ -0,0 +1,52 @@
+import javax.enterprise.inject.Alternative;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+
+@H2Transaction
+@Alternative
+public class H2UserDao implements UserHandler {
+    private EntityManager entityManager;
+
+    public H2UserDao() {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("USER");
+        entityManager = factory.createEntityManager();
+    }
+
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    @Override
+    public User create(String email, String password, UserType type) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setType(type);
+        entityManager.persist(user);
+        return user;
+    }
+
+    @Override
+    public boolean update(User user) {
+        entityManager.persist(user);
+        return find(user.getId()) != null;
+    }
+
+    @Override
+    public User find(int id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public List getAll() {
+        return entityManager.createNamedQuery("getAllUsers").getResultList();
+    }
+
+    @Override
+    public boolean delete(int id) {
+        entityManager.remove(find(id));
+        return find(id) == null;
+    }
+}
